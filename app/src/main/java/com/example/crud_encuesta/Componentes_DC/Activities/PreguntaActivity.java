bb627abacb63007c01c6ccc0f1f9a6ae15a2350c
Enrique <mt16007@ues.edu.sv>
2019-06-12 12:52:54 -0600

    Importar desde excel y exportar hacia excel

--- app/src/main/java/com/example/crud_encuesta/Componentes_DC/Activities/PreguntaActivity.java
+++ app/src/main/java/com/example/crud_encuesta/Componentes_DC/Activities/PreguntaActivity.java
@@ -1,8 +1,14 @@
 package com.example.crud_encuesta.Componentes_DC.Activities;
 
+import android.Manifest;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,6 +16,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -21,10 +28,14 @@ import com.example.crud_encuesta.Componentes_DC.Dao.DaoPregunta;
 import com.example.crud_encuesta.Componentes_DC.Objetos.Pregunta;
 import com.example.crud_encuesta.Componentes_MT.Area.Area;
 import com.example.crud_encuesta.Componentes_MT.Area.AreaActivity;
+import com.example.crud_encuesta.Componentes_MT.Excel.Excel;
 import com.example.crud_encuesta.R;
 import com.example.crud_encuesta.SubMenuMateriaActivity;
+import com.nbsp.materialfilepicker.MaterialFilePicker;
+import com.nbsp.materialfilepicker.ui.FilePickerActivity;
 
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 public class PreguntaActivity extends AppCompatActivity {
 
@@ -43,6 +54,13 @@ public class PreguntaActivity extends AppCompatActivity {
     private ImageView btn_buscar;
     private ImageView btn_todo;
     private EditText texto_busqueda;
+
+    //----------------------------------Proyecto de investigacion---------------------------------//
+    private ImageButton excelUpload;
+    private ImageButton excelDownload;
+    private String path_excel="";
+    //----------------------------------------------End-------------------------------------------//
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -160,6 +178,36 @@ public class PreguntaActivity extends AppCompatActivity {
             }
         });
 
+        //--------------------------------Proyecto de investigacion-------------------------------//
+        excelDownload = findViewById(R.id.excelDownload);
+        excelUpload = findViewById(R.id.excelUpload);
+
+        //Si no viene de grupo emparejamiento poner objetos a la escucha del evento
+        if(id_gpo_emp==0){
+            excelUpload.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    permisoAlmacenamiento();
+                    cargarExcel();
+                }
+            });
+
+            excelDownload.setOnClickListener(new View.OnClickListener(){
+
+                @Override
+                public void onClick(View v) {
+                    permisoAlmacenamiento();
+                    descargarExcel();
+
+                }
+            });
+            // Si viene de grupo emparejamiento ocultar los iconos del activity pregunta
+        }else {
+            excelDownload.setVisibility(View.GONE);
+            excelUpload.setVisibility(View.GONE);
+        }
+        //------------------------------------------End-------------------------------------------//
+
     }
 
     public void onBackPressed()
@@ -176,4 +224,97 @@ public class PreguntaActivity extends AppCompatActivity {
             finish();
         }*/
     }
+
+    //----------------------------------Proyecto de investigacion---------------------------------//
+    //Resultado de elegir el archivo desde el almacenamiento
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1000 && resultCode == RESULT_OK) {
+            path_excel = data.getStringExtra(FilePickerActivity.RESULT_FILE_PATH);
+            //Toast.makeText(this, path_excel, Toast.LENGTH_SHORT).show();
+            Excel excel = new Excel(this);
+            if(excel.leerExcel(path_excel, id_area)>0){
+                Toast.makeText(this, "Se ha importado conéxito", Toast.LENGTH_SHORT).show();
+                adaptadorRV.notifyDataSetChanged();
+                lista_preguntas = dao.verTodos();
+            }else{
+                Toast.makeText(this, "Hubo un error o el documento está vacío", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
+    //Resultado de si se acepto o no el permiso de almacenamiento en el dispositivo
+    @Override
+    public void onRequestPermissionsResult (int requestCode, String[] permissions, int[] grantResults){
+        switch (requestCode){
+            case 1001:{
+                if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this, "Permiso concedido", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(this, "Permiso denegado", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        }
+    }
+
+    //Consultar permiso para leer y escribir en el dispositivo
+    public void permisoAlmacenamiento(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        1001);
+            }
+        }
+    }
+
+    //Guardar excel en el dispositivo
+    public void descargarExcel(){
+        final EditText etFilename = new EditText(this);
+
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(this);
+
+        mBuilder.setMessage("Ingrese el nombre con el que desee guardar el archivo excel");
+        mBuilder.setIcon(R.drawable.infoazul);
+        mBuilder.setTitle("Excel");
+        mBuilder.setView(etFilename);
+
+        mBuilder.setPositiveButton(R.string.aceptar, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String filename = etFilename.getText().toString();
+                Excel excel = new Excel(PreguntaActivity.this);
+                excel.crearExcel(PreguntaActivity.this, filename);
+            }
+        });
+
+        mBuilder.setNegativeButton(R.string.mt_cancelar, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        AlertDialog dialog = mBuilder.create();
+        dialog.show();
+    }
+
+    //Subir preguntas desde el dispositivo
+    public void cargarExcel(){
+        new MaterialFilePicker()
+                .withActivity(PreguntaActivity.this)
+                .withRequestCode(1000)
+                .withFilter(Pattern.compile(".*\\.xls$")) // Filtering files and directories by file name using regexp
+                // .withFilterDirectories(true) // Set directories filterable (false by default)
+                .withHiddenFiles(true) // Show hidden files and folders
+                .start();
+    }
+    //----------------------------------------------End-------------------------------------------//
 }
