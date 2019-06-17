37642a71eba2c50ccfe552f0193f2813e7543033
renemarvera <renemarvera@gmail.com>
2019-06-17 05:38:33 -0600

    Index de ListView como Busqueda :'(

--- app/src/main/java/com/example/crud_encuesta/Componentes_MR/Estudiante/ActivityEstudiante.java
+++ app/src/main/java/com/example/crud_encuesta/Componentes_MR/Estudiante/ActivityEstudiante.java
@@ -1,13 +1,12 @@
 package com.example.crud_encuesta.Componentes_MR.Estudiante;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.DialogInterface;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -17,6 +16,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,6 +25,9 @@ import com.example.crud_encuesta.Componentes_MR.Funciones;
 import com.example.crud_encuesta.DatabaseAccess;
 import com.example.crud_encuesta.R;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.Vector;
 
@@ -41,11 +44,14 @@ public class ActivityEstudiante extends AppCompatActivity {
 
     private DAOEstudiante dao;
     private AdaptadorEstudiante adapter;
-    private ArrayList<Estudiante> lista;
+    private ArrayList<Estudiante> lista = new ArrayList<>();
     private Estudiante estudiante;
     private Usuario usuario;
     private SQLiteDatabase db;
     private DatabaseAccess access;
+
+    Map<String, Integer> mapIndex;
+    private ListView list;
 
     Session session = null;
     String rec;
@@ -57,11 +63,19 @@ public class ActivityEstudiante extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
 
+        ArrayList<String> fruits = new ArrayList<>();
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estudiante);
 
         dao = new DAOEstudiante(this);
         lista = dao.verTodos();
+
+        for (int i=0;i<lista.size();i++){
+            fruits.add(lista.get(i).getCarnet());
+        }
+
         adapter = new AdaptadorEstudiante(this,lista,dao);
         access = DatabaseAccess.getInstance(ActivityEstudiante.this);
         db = access.open();
@@ -106,6 +120,9 @@ public class ActivityEstudiante extends AppCompatActivity {
                 }
             }
         });
+
+        getIndexList(fruits);
+        displayIndex(list);
 
         /*Botón para Nuevo Estudiante*/
         agregar.setOnClickListener(new View.OnClickListener() {
@@ -272,6 +289,41 @@ public class ActivityEstudiante extends AppCompatActivity {
         @Override
         protected void onPostExecute(String result) {
             Toast.makeText(getApplicationContext(), "¡Estudiante y usuario creados!\nCorreo Electrónico Enviado.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void getIndexList(ArrayList<String> fruits) {
+        mapIndex = new LinkedHashMap<String, Integer>();
+        for (int i = 0; i < fruits.size(); i++) {
+            String fruit = fruits.get(i);
+            String index = fruit.substring(0, 1).toUpperCase();
+
+            if (mapIndex.get(index) == null)
+                mapIndex.put(index, i);
+        }
+    }
+
+    private void displayIndex(final ListView list) {
+        LinearLayout indexLayout = (LinearLayout) findViewById(R.id.side_index);
+
+        TextView textView;
+        List<String> indexList = new ArrayList<String>(mapIndex.keySet());
+        for (String index : indexList) {
+            textView = (TextView) getLayoutInflater().inflate(
+                    R.layout.side_index_item, null);
+            textView.setText(index);
+            final TextView finalTextView = textView;
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    lista = dao.verBusquedaIndex(finalTextView.getText().toString());
+                    if((lista != null) && (lista.size() > 0)){
+                        list.setAdapter(adapter);
+                    }
+                }
+            });
+            indexLayout.addView(textView);
         }
     }
 }
